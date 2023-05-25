721adea6e2707aa6f12ed0531c6e8a31ecf63eb9
Nouman <noumanmufc07@gmail.com>
2023-05-25 11:03:37 +0200

    add module for visual qa

--- app/app.py
+++ app/app.py
@@ -8,9 +8,19 @@ import shutil
 import sys
 sys.path.insert(1, "src/models")
 from extractive_qa import QA
+from visual_qa import VisualQA
 from search_engine import IR
 # from src.models.extractive_qa import QA
 # from src.models.search_engine import IR
+
+
+@st.cache_resource
+def load_visual_qa_module():
+    """
+    Loads the Visual QA module
+    """
+    qa_module = VisualQA()
+    return qa_module
 
 @st.cache_resource
 def load_qa_module():
@@ -28,6 +38,14 @@ def load_search_engine():
     search_engine = IR()
     return search_engine
 
+def get_metadata_from_question(question):
+    if 'artist' in question:
+        return 'artist'
+    elif 'style' in question:
+        return 'style'
+    elif 'genre' in question:
+        return 'genre'
+
 # Defining a session variable
 if 'extractive_qa' not in st.session_state:
 	st.session_state.extractive_qa = False
@@ -38,6 +56,7 @@ model_path = Path.cwd() / 'models'
 if dirpath.exists() and dirpath.is_dir():
     shutil.rmtree(dirpath)
 
+vqa_module = load_visual_qa_module()
 qa_module = load_qa_module()
 search_engine = load_search_engine()
 
@@ -52,7 +71,7 @@ st.markdown("""Hello, please take a picture of the painting and ask a question a
 imgbuffer = st.camera_input('')
 
 # Prompt for a question
-question = st.text_input(label="What is your question (e.g. Who's the author of the painting?")
+question = st.text_input(label="What is your question (e.g. Who's the artist of this painting?)")
 
 if question:
     print(f'Received question: {question}')
@@ -63,25 +82,18 @@ if question:
         print(f'Found {len(articles)} search results')
         
         if len(articles) == 0:
-            st.write("Sorry, I don't know the answer to that question :(")
+            st.markdown("Sorry, I don't know the answer to that question :(")
         else:
             best_result = articles[0]
             answer = qa_module.answer_question(question, best_result)
-            st.write(answer)
+            st.markdown(f'Answer: {answer}')
     else:
         # Doing VQA
-        st.write('answer to vqa')
+        img = Image.open(imgbuffer)
+
+        result = vqa_module.answer_question(question, img)
+        meta_data = get_metadata_from_question(question)
+        st.markdown(f"Answer: The {meta_data} of this painting is {result}")
 
         # Switching to extractive QA
         st.session_state.extractive_qa = True
-
-if imgbuffer:
-    img = Image.open(imgbuffer)
-    imgnp = np.array(img)
-    #st.write(imgnp.shape)
-
-    # perform inference
-    results = model(img, size=640)
-    st.markdown("The artist of this painting is Leonardo Da Vinci")
-
-    # Information extraction
---
+++ src/models/visual_qa.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+
+
+class VisualQA(object):
+    def __init__(self, model_name='nouman-10/vqa_test', tokenizer_name='dandelin/vilt-b32-finetuned-vqa'):
+        self.model_name = model_name
+        self.tokenizer_name = tokenizer_name
+        self.__load_model()
+
+    def __load_model(self):
+        self.model = pipeline('vqa', model=self.model_name, tokenizer=self.tokenizer_name)
+
+    def answer_question(self, query, image):
+        return self.model(question=query, image=image, top_k=1)[0]['answer']
